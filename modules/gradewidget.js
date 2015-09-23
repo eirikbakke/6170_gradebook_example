dc7a0e1dbef718ab99ec0001c574d9249be69e82
Eirik Bakke <ebakke@mit.edu>
2015-09-22 21:21:47 -0400

    Now add support for editing the grades. The user can click on a grade to change it, pressing enter or tab or another grade to save the change. (Note that there is no actual persistence in this example; changes are just in-memory.)

--- modules/gradewidget.js
+++ modules/gradewidget.js
@@ -18,8 +18,35 @@ GradeWidget_install =
   domContainer.append($("<h2>Grades by Assignment</h2>"));
   domContainer.append(dropdownElm);
   domContainer.append(tableElm);
+  var gradeInputElm = $("<input type='number'>");
+  var currentlyEditedRowKey = null;
+  var acceptCurrentEdit = function() {
+    if (currentlyEditedRowKey !== null) {
+      var student = currentlyEditedRowKey;
+      currentlyEditedRowKey = null;
+      var assignment = dropdownElm.val();
+      var gradeInput = gradeInputElm.val();
+      if (gradeInput.length > 0) {
+        gradebook.setGrade(student, assignment, parseInt(gradeInput));
+      }
+      rebuild_table();
+    }
+  };
+  gradeInputElm.keydown(function (evt) {
+    // Enter or tab keys.
+    if (evt.keyCode === 13 || evt.keyCode === 9) {
+      // Avoid the default focus-traversing behavior of the tab key.
+      evt.preventDefault();
+      acceptCurrentEdit();
+    }
+  });
 
   var rebuild_table = function() {
+    /* jQuery will automatically detach any listeners when an element is removed
+    from the DOM. Use detach() to remove gradeInputElm without removing its
+    listeners, since we'll be reusing it. See
+    http://stackoverflow.com/questions/12528049/if-a-dom-element-is-removed-are-its-listeners-also-removed-from-memory . */
+    gradeInputElm.detach();
     var newTableElm = $("<table class='GradeWidget'>");
     var dropdownValue = dropdownElm.val();
     var total = 0, N = 0;
@@ -28,7 +55,24 @@ GradeWidget_install =
         var grade = gradebook.getGrade(rowKey, dropdownValue);
         var tableRow = $("<tr>");
         tableRow.append($("<td>", { text: rowKey }));
-        tableRow.append($("<td>", { text: grade }));
+        var gradeCell = $("<td>");
+        if (rowKey !== currentlyEditedRowKey) {
+          var gradeElm = $("<a>", { text: grade, href: "#" });
+          gradeCell.append(gradeElm);
+          gradeCell.click(function() {
+            acceptCurrentEdit();
+            currentlyEditedRowKey = rowKey;
+            rebuild_table();
+            if (gradeInputElm !== null) {
+              gradeInputElm.select();
+              gradeInputElm.focus();
+            }
+          });
+        } else {
+          gradeCell.append(gradeInputElm);
+          gradeInputElm.val(grade);
+        }
+        tableRow.append(gradeCell);
         newTableElm.append(tableRow);
         total += grade; N++;
       });
